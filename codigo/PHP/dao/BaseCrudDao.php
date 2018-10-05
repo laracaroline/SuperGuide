b04389cb3eeed644702aa484ee047d363ef080ac
laracaroline <laracaroline2007@hotmail.com>
2018-10-05 20:39:29 -0300

    DAO
    Base, conexao e inicio do CRUD pais

---
+++ codigo/PHP/dao/BaseCrudDao.php
@@ -0,0 +1,8 @@
+<?php
+interface BaseCrudDao {
+    public function create($object);
+    public function read($param);
+    public function update($object);
+    public function delete($param);
+}
+?>
