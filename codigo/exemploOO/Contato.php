31d83fc23c42fdd63fcedde3ff524b8f84b44437
willianIF <30538522+willianIF@users.noreply.github.com>
2018-10-05 20:18:19 -0300

    Exemplo Lucas

---
+++ codigo/exemploOO/Contato.php
@@ -0,0 +1,46 @@
+<?php
+class Contato {
+    private $id = null;
+    private $nome;
+    private $email;
+    private $telefone;
+
+    public function __construct($nome, $email, $telefone) {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->telefone = $telefone;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getTelefone() {
+        return $this->telefone;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+}
+?>
