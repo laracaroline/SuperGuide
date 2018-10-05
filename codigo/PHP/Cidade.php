796c6152220b06e98d26d09d3282d65ecfa43c5f
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2018-10-05 19:41:22 -0300

    Arrumando o Cidade.php

--- codigo/PHP/Cidade.php
+++ codigo/PHP/Cidade.php
@@ -1,28 +1,28 @@
 <?php
 class Cidade{
-	private $id_cidade;
-	private $nome_cidade;
+	private $id;
+	private $nome;
 	private $id_estado;
 	
-	public function __construct($id_cidade,$nome_cidade,$id_estado){
-		$this->id_cidade = $id_cidade;
-		$this->nome_cidade = $nome_cidade;
+	public function __construct($id,$nome,$id_estado){
+		$this->id = $id;
+		$this->nome = $nome;
 		$this->id_estado = $id_estado;
 	}
-	public function getIdCidade(){
-		return $this->id_cidade;
+	public function getId(){
+		return $this->id;
 	}
-	public function getNomeCidade(){
-		return $this->nome_cidade;
+	public function getNome(){
+		return $this->nome;
 	}
 	public function getIdEstado(){
 		return $this->id_estado;
 	}
-	public function setIdCidade($id_cidade){
-		$this->id_cidade = $id_cidade;
+	public function setId($id){
+		$this->id = $id;
 	}
-	public function setNomeCidade($nome_cidade){
-		$this->nome_cidade = $nome_cidade;
+	public function setNome($nome){
+		$this->nome = $nome;
 	}
 	public function setIdEstado($id_estado){
 		$this->id_estado = $id_estado;
