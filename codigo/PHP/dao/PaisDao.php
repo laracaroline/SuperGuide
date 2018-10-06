e18a76616c9bde146fbbaa4483fbbd3558b78d67
laracaroline <laracaroline2007@hotmail.com>
2018-10-05 22:14:24 -0300

    correção

--- codigo/PHP/dao/PaisDao.php
+++ codigo/PHP/dao/PaisDao.php
@@ -9,18 +9,19 @@
       private $tabela;
 
       function __construct(){
-        $this->instanciaConexaoPdo = ConexaoPdo::getInstancia();
+        $this->instanciaConexaoPdo = Conexao::getInstancia();
         $this->tabela = "paises";
       }
 
       public function create($pais){
-          $nome = $pais->$getNome();
+          $nome = $pais->getNome();
 
           $sqlStmt = "INSERT INTO {$this->tabela} (nome_pais) VALUES (:nome)";
 
           try{
               $operacao = $this->instanciaConexaoPdo->prepare($sqlStmt);
               $operacao->bindValue(":nome", $nome, PDO::PARAM_STR);
+
           }catch(PDOException $excecao){
             echo $excecao->getMessage();
           }
