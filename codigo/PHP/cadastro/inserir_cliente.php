0be4dbf0853f85273ccc7181acf3496800685e2e
Lara Caroline <laracaroline2007@hotmail.com>
2018-11-15 21:50:53 -0200

    Cadastro do cliente funcionando

---
+++ codigo/PHP/cadastro/inserir_cliente.php
@@ -0,0 +1,26 @@
+<?php
+  include_once '../dao/ClienteDao.php';
+  include_once '../Cliente.php';
+  include_once '../dao/CidadeDao.php';
+
+  $nome = $_POST["nome"];
+  $cpf = $_POST["cpf"];
+  $telefone = $_POST["telefone"];
+  $email = $_POST["email"];
+  $cidade = $_POST["cidade"];
+  $dataNascimento = $_POST["data_nasc"];
+  $senha = $_POST["senha"];
+
+  //echo $cidade;
+  $cidadeDao = new CidadeDao();
+  $cidadeObjeto = $cidadeDao->read($cidade);
+  $cliente = new Cliente($nome, $cpf, $telefone, $email, $senha, $dataNascimento, $cidadeObjeto);
+  $clienteDao = new ClienteDao();
+  
+  if($clienteDao->create($cliente)){
+    echo "Cliente inserido com sucesso!";
+  } else{
+    echo "Falha!";
+  }
+
+ ?>
