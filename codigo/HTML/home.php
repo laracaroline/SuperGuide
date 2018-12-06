449471292e3fbfcd6b4483daa2b09a8ef5668505
aandreysr <aandreysr@gmail.com>
2018-12-06 02:08:59 -0200

    home

--- codigo/HTML/home.php
+++ codigo/HTML/home.php
@@ -2,8 +2,35 @@
 <html>
 	<head>
 	<title>Pagina inicial</title>
+	<link rel="short cut icon" type="image/png" href="src/img/icon.png">
 
+	<link rel="stylesheet" type="text/css" href="src/semantic.min.css">
+	<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+	<script src="src/semantic.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="src/css/style_home.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body>
+		<header>
+			<nav>
+				<div class="cabecalho">
+					<img src="src/img/logo.png" id="tamlogo">
+
+					<div id="divBusca">
+						<img src="src/img/buscar.png" alt="Buscar..."/>
+					  <input type="text" id="txtBusca" placeholder="Buscar..."/>
+					  <button id="btnBusca">Buscar</button>
+					</div>
+
+				</div>
+			</nav>
+		</header>
   </body>
 </html>
--- codigo/HTML/index.php
+++ codigo/HTML/index.php
@@ -20,24 +20,24 @@
 	<head>
 	<title>Login</title>
 
-	<link rel="stylesheet" type="text/css" href="../HTML/src/semantic.min.css">
-	<script
+	<link rel="stylesheet" type="text/css" href="src/semantic.min.css">
+	<script>
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
   crossorigin="anonymous"></script>
-	<script src="../HTML/src/semantic.min.js"></script>
+	<script src="src/semantic.min.js"></script>
 
-	<link rel="stylesheet" type="text/css" href="../HTML/src/css/style_login.css">
+	<link rel="stylesheet" type="text/css" href="src/css/style_login.css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/jquery.mask.min.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/mascaras.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/script_index.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/additional-methods.min.js"></script>
-	<script type="text/javascript" src="../HTML/src/js/localization/messages_pt_BR.min.js"></script>
+	<script type="text/javascript" src="src/js/jquery.mask.min.js"></script>
+	<script type="text/javascript" src="src/js/mascaras.js"></script>
+	<script type="text/javascript" src="src/js/script_index.js"></script>
+	<script type="text/javascript" src="src/js/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="src/js/additional-methods.min.js"></script>
+	<script type="text/javascript" src="src/js/localization/messages_pt_BR.min.js"></script>
 	</head>
 
 	<body>
